211756a174ff06b2f170b7dbe19e58a1074f4c4d
Nojembre <nojembre@gmail.com>
2020-02-22 13:41:39 -0600

    Batch Files for testing
    Double click the batch files and it'll change everything over to a version with quicker tech research and more money.

---
+++ Data/ReleasetoTest.bat
@@ -0,0 +1,8 @@
+cd /d "%~dp0"
+cd XML
+rename TechBuilding.xml TechBuildingRelease.xml
+rename TechBuildingTest.xml TechBuilding.xml
+rename Gameconstants.xml GameconstantsRelease.xml
+rename GameconstantsTest.xml Gameconstants.xml
+rename Upgradeobjects.xml UpgradeobjectsRelease.xml
+rename UpgradeobjectsTest.xml Upgradeobjects.xml
---
+++ Data/TesttoRelease.bat
@@ -0,0 +1,8 @@
+cd /d "%~dp0"
+cd XML
+rename TechBuilding.xml TechBuildingTest.xml
+rename TechBuildingRelease.xml TechBuilding.xml
+rename Gameconstants.xml GameconstantsTest.xml
+rename GameconstantsRelease.xml Gameconstants.xml
+rename Upgradeobjects.xml UpgradeobjectsTest.xml
+rename UpgradeobjectsRelease.xml Upgradeobjects.xml
